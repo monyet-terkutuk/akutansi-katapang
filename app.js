8474e7cbbe44a83456a07a2b1e71672dfb60e03f
@undeath-cyber <gabrielxjonathan.12@gmail.com>
2024-08-31 09:37:21 +0700

    fix: cors

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+// app.use(
+//   cors(
+//   )
+// );
+
 app.use(
-  cors(
-  )
+  cors({
+    origin: "https://administrasi-kec-katapang.vercel.app", // Ganti dengan domain front-end Anda
+    methods: ["GET", "POST", "PUT", "DELETE"], // Metode HTTP yang diizinkan
+    credentials: true, // Mengizinkan cookie dikirim dalam permintaan lintas domain
+  })
 );
+
 
 app.use(express.json());
 app.use(cookieParser());
